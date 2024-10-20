a523bfb9ad5f862efbba36599df4f37e0672da96
mxnuchim <manuchimoliver779@gmail.com>
2024-10-20 07:08:31 +0100

    ✅ Starter template complete

--- app/(content)/layout.tsx
+++ app/(content)/layout.tsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import Footer from "@/components/navigation/footer";
+import Navbar from "@/components/navigation/navbar";
+import React from "react";
 
 interface Props {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
-const MarketingLayout = ({ children }: Props) => {
-    return (
-        <div className="flex flex-col items-center w-full">
-            {children}
-        </div>
-    )
+const ContentLayout = ({ children }: Props) => {
+  return (
+    <div className="flex flex-col items-center w-full">
+      <Navbar />
+      {children}
+      <Footer />
+    </div>
+  );
 };
 
-export default MarketingLayout
+export default ContentLayout;
