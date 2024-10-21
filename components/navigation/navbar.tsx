46593f082d19d84fa6ead6952a97635105b870ee
mxnuchim <manuchimoliver779@gmail.com>
2024-10-21 01:26:10 +0100

    ✅ Starter template complete

--- components/navigation/navbar.tsx
+++ components/navigation/navbar.tsx
@@ -1,7 +1,20 @@
+import Link from "next/link";
 import React from "react";
+import Icons from "../global/icons";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className="px-4 h-14 sticky top-0 inset-x-0 w-full bg-background/40 backdrop-blur-lg border-b border-border z-50">
+      <div className="flex items-center justify-between h-full mx-auto md:max-w-screen-xl">
+        <div className="flex items-start">
+          <Link href={"/"} className="flex items-center gap-2">
+            <Icons.logo className="h-8 w-8" />
+            <span className="text-lg font-medium">Nexa</span>
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
